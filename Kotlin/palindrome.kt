ac96ecf03ac2ede2cf3f29381f18a816e61ca24c
AAlbentosa <83396539+AAlbentosa@users.noreply.github.com>
2021-09-28 23:50:08 +0200

    Delete Kotlin directory

--- Kotlin/palindrome.kt
+++
@@ -1,9 +0,0 @@
-fun esPalindromo(palabra: String):Boolean {
-
-        for (y in 0..palabra.length-1) {
-             if(palabra[y]!=palabra[(palabra.length-1)-y]){
-                return false
-             }
-         }
-         return true
-}
