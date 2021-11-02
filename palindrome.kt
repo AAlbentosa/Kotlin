10e1d4448cc32d2dc1eee3a5452cb0bd25542736
AAlbentosa <83396539+AAlbentosa@users.noreply.github.com>
2021-11-02 16:44:08 +0100

    Update palindrome.kt

--- palindrome.kt
+++ palindrome.kt
@@ -1,9 +1,8 @@
 fun isPalindrome(word: String):Boolean {
 
-        for (y in 0..word.length-1) {
-             if(word[y]!=word[(word.length-1)-y]){
-                return false
-             }
+         for (y in 0..word.length-1) {
+                if(word[y]!=word[(word.length-1)-y])
+                        return false
          }
          return true
 }
