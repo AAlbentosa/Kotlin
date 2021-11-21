807a21ebde2ea43adad91b0e4bdc6643d9ee0be6
AAlbentosa <83396539+AAlbentosa@users.noreply.github.com>
2021-11-22 00:47:35 +0100

    Delete palindrome.kt

--- palindrome.kt
+++
@@ -1,8 +0,0 @@
-fun isPalindrome(word: String):Boolean {
-
-         for (y in 0..word.length-1) {
-                if(word[y]!=word[(word.length-1)-y])
-                        return false
-         }
-         return true
-}
